377e017b5903de908707ef7ea508596ec65ee262
김현우 <hyun0310woo@gmail.com>
2020-07-14 00:53:32 +0900

    Login & Sign up page

--- self/src/App.js
+++ self/src/App.js
@@ -1,21 +1,20 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useRouteMatch,
-  useParams,
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link,
+//   useRouteMatch,
+//   useParams,
+// } from "react-router-dom";
 import Login from "./components/Login";
-import Signup from "./components/Signup";
+// import Signup from "./components/Signup";
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <Login />
-        <Signup />
       </div>
     );
   }
